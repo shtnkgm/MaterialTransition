6cea68d9ccf0708de7f4534e8b7280217815656e
shtnkgm <shtnkgm@gmail.com>
2019-04-01 23:11:44 +0900

    fix

--- MaterialTransition/ListViewCell.swift
+++ MaterialTransition/ListViewCell.swift
@@ -13,5 +13,10 @@ class ListViewCell: UICollectionViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        layer.cornerRadius = 20
+//        layer.shadowOffset = CGSize(width: 0, height: 5)
+//        layer.shadowRadius = 5
+//        layer.shadowOpacity = 0.8
+//        clipsToBounds = false
     }
 }
